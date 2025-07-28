fb455071c72206d2803375cabab4a3c108043389
Ava Gaiety W. <ava.gaiety@revzilla.com>
2025-07-28 13:38:37 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Codespaces Dotfiles based on: https://github.com/dcreager/dotfiles/blob/main/install
+
+if [ -z "$USER" ]; then
+    USER=$(id -un)
+fi
+
+echo >&2 "====================================================================="
+echo >&2 " Setting up codespaces environment"
+echo >&2 ""
+echo >&2 " USER        $USER"
+echo >&2 " HOME        $HOME"
+echo >&2 "====================================================================="
+
+cd $HOME
+
+# Make passwordless sudo work
+export SUDO_ASKPASS=/bin/true
+
+# I'd like to use fish, please
+sudo apt-get install -y fish
+sudo chsh -s /usr/bin/fish $USER
+
+# Install fzf
+FZF_VERSION=0.30.0
+curl -L https://github.com/junegunn/fzf/releases/download/${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz | tar xzC $HOME/bin
+
+# Install neovim
+NVIM_VERSION=0.7.0
+sudo apt-get install -y libfuse2
+curl -L -o $HOME/bin/nvim https://github.com/neovim/neovim/releases/download/v${NVIM_VERSION}/nvim.appimage
+chmod a+x $HOME/bin/nvim
+
+# Personal Dotfiles
+git clone https://git.basking.monster/gaiety/dotfiles.git dotfiles
+cd dotfiles
+ln -s "$(pwd)/nvim" ~/.config/nvim
+ln -s "$(pwd)/fish" ~/.config/fish
+cd $HOME
