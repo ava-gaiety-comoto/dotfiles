aba0706f89fbda90fbe9808262b47c75ec0c9f28
Gaiety <ava+git@gaiety.me>
2026-08-19 10:21:24 -0600

    clean and mostly reset dotfiles

--- README.md
+++ README.md
@@ -1,17 +1,3 @@
 # dotfiles
 For Codespaces
 https://docs.github.com/codespaces/setting-your-user-preferences/personalizing-github-codespaces-for-your-account#dotfiles
-
-Adds `task`, `bat`, `fzf`, my personal dotfiles for NeoVim etc
-
-Useful Comoto workflow git aliases:
-
-- `git start NERD-xxx` creates a properly named branch
-- `git ticket NERD-xxx` switches to a branch regardless of what user made it / the branch prefix
-
-## Forking this Repository
-
-1. Fork the repository
-2. Change `.gitconfig` to match your own user
-3. Change `setup.sh`'s final lines to pull in _your_ own dotfiles, or remove that step altogether unless you want my private subject-to-change configs
-4. [Connect your fork to GitHub CodeSpaces](https://github.com/settings/codespaces)
--- install.sh
+++ install.sh
@@ -1,38 +1,21 @@
 #!/bin/sh
 
-# Codespaces Dotfiles based on: https://github.com/dcreager/dotfiles/blob/main/install
-
 # Prepare to install dependencies
 sudo apk update
-sudo apk add zellij zoxide bat mise
+sudo apk add zoxide bat mise navi
 
 # Upgrade NeoVim
 sudo apk add --upgrade --repository=https://dl-cdn.alpinelinux.org/alpine/edge/community neovim
-
-# basic dotfiles config
-cp ./.zshrc $HOME/.zshrc
-cp ./.gitconfig $HOME/.gitconfig
-
-# Personal Dotfiles
-git clone -b 2026 https://git.basking.monster/gaiety/dotfiles.git $HOME/dotfiles
-cd $HOME/dotfiles
-chmod +x ./mise-tasks/*
-mise trust
-mise install
-mise run install-ohmyzsh
-ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
-ln -sf "$(pwd)/nvim" ~/.config/nvim
-ln -sf "$(pwd)/.zshrc" ~/.zshrc
 
 # Upgrade FZF
 sudo apk del fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 bash ~/.fzf/install --all
 
-# Comoto Helpers
-# git clone https://github.com/ava-gaiety-comoto/comoto-shell-helpers.git $HOME/comoto-shell-helpers
-# cd $HOME/comoto-shell-helpers
-# chmod +x $HOME/comoto-shell-helpers/**/*.sh
-# echo 'alias c="$HOME/comoto-shell-helpers/scripts.sh"' >> ~/.zshrc
-# echo 'alias cr="sh $HOME/comoto-shell-helpers/history-read.sh"' >> ~/.zshrc
-# echo 'alias cR="sh $HOME/comoto-shell-helpers/history-read.sh -t"' >> ~/.zshrc
+# chmod +x ./mise-tasks/*
+# mise trust
+# mise install
+ln -sf "$(pwd)/.gitconfig" ~/.gitconfig
+# ln -sf "$(pwd)/nvim" ~/.config/nvim
+# ln -sf "$(pwd)/.zshrc" ~/.zshrc
+# ln -sf "$(pwd)/.cheat" ~/.cheat
